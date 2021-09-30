4acc9d3df02e12a059ef20fd9c26d738432cb04a
Piotr Duda <ptd.duke@gmail.com>
2021-09-30 16:27:24 +0200

    add newsletter functonality

---
+++ pages/api/newsletter.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+function getRequestParams(email) {
+  // get env variables
+  const API_KEY = "cf33f4dbcb7b1082cdb480ea6bb16ff4-us5";
+  const LIST_ID = "99651a52cf";
+  // mailchimp datacenter - mailchimp api keys always look like this:
+  // fe4f064432e4684878063s83121e4971-us6
+  // We need the us6 part
+  const DATACENTER = API_KEY.split("-")[1];
+
+  const url = `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${LIST_ID}/members?skip_merge_validation=true`;
+
+  // Add aditional params here. See full list of available params:
+  // https://mailchimp.com/developer/reference/lists/list-members/
+  const data = {
+    email_address: email,
+    status: "subscribed",
+  };
+
+  // Api key needs to be encoded in base 64 format
+  const base64ApiKey = Buffer.from(`anystring:${API_KEY}`).toString("base64");
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Basic ${base64ApiKey}`,
+  };
+
+  return {
+    url,
+    data,
+    headers,
+  };
+}
+
+export default async (req, res) => {
+  const { email } = req.body;
+
+  if (!email || !email.length) {
+    return res.status(400).json({
+      error: "Forgot to add your email?",
+    });
+  }
+
+  try {
+    const { url, data, headers } = getRequestParams(email);
+
+    const response = await axios.post(url, data, { headers });
+
+    // Success
+    return res.status(201).json({ error: null });
+  } catch (error) {
+      return res.status(400).json({ erro: error.message});
+    console.error(error);
+    // Report error to Sentry or whatever
+  }
+};
---
+++ pages/index.js
@@ -0,0 +1,135 @@
+import Head from 'next/head'
+import { useState } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [state, setState] = useState("IDLE");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const subscribe = async () => {
+    setState("LOADING");
+    setErrorMessage(null);
+    try {
+      const response = await axios.post("/api/newsletter", { email });
+      setState("SUCCESS");
+    } catch (e) {
+      setErrorMessage(e.response.data.error);
+      setState("ERROR");
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{' '}
+          <a className="text-blue-600" href="https://nextjs.org">
+            Next.js!
+          </a>
+        </h1>
+
+        <p className="mt-3 text-2xl">
+          Get started by editing{' '}
+          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
+            pages/index.js
+          </code>
+        </p>
+
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+          <a
+            href="https://nextjs.org/docs"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Find in-depth information about Next.js features and API.
+            </p>
+          </a>
+
+          <a
+            href="https://nextjs.org/learn"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Learn about Next.js in an interactive course with quizzes!
+            </p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Discover and deploy boilerplate example Next.js projects.
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+
+        <div className="flex flex-col items-center w-full p-8 border-gray-500 border-solid border rounded-sm mt-8">
+      <h2 className="text-3xl font-bold text-center">
+        I also have a newsletter!
+      </h2>
+      <p className="mt-2 font-light w-4/5 text-center leading-relaxed">
+        It includes intersting stuff about tech and will arrive to your mailbox
+        no more than once every 2 weeks
+      </p>
+      <div className="flex w-1/2 lg:w-2/3 justify-center mt-5 flex-col lg:flex-row">
+        <input
+          className="appearance-none mb-2 lg:mb-0 w-full lg:w-2/3 border border-gray-500 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-gray-600"
+          type="text"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button
+          className={`lg:ml-2 w-full lg:w-1/3 shadow bg-brand2 focus:shadow-outline focus:outline-none text-center text-gray-600 font-bold py-2 px-4 rounded flex ${
+            state === "LOADING" ? "button-gradient-loading" : ""
+          }`}
+          type="button"
+          disabled={state === "LOADING"}
+          onClick={subscribe}
+        >
+          Subscribe
+        </button>
+      </div>
+      {state === "ERROR" && (
+        <p className="w-1/2 mt-2 text-red-600">{errorMessage}</p>
+      )}
+      {state === "SUCCESS" && (
+        <p className="w-1/2 mt-2 text-green-600">Success!</p>
+      )}
+    </div>
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <a
+          className="flex items-center justify-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+        </a>
+      </footer>
+    </div>
+  )
+}
